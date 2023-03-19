7c3f7a705fcb12e0d54892317f6703dda2efe47c
Carlos Ramos <ramoscarlos@192.168.0.227>
2023-03-19 11:02:12 -0600

    Change to renamed app

--- quick_draft_letrista/wsgi.py
+++ quick_draft_letrista/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_letrista.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quick_draft_letrista.settings')
 
 application = get_wsgi_application()
